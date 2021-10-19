7fa2da4986b553b0f026f8958f3f55cd46f21eba
erokeen <70420838+erokeen@users.noreply.github.com>
2021-10-19 15:16:04 -0500

    Update create-automationaccount.ps1
    Corrected region- Automation account need to be in the correct region (eastus2)

--- create-automationaccount.ps1
+++ create-automationaccount.ps1
@@ -1,2 +1,2 @@
 #Create Azure Automation Account in your Azure resource groups.
-New-AzureRmAutomationAccount -Name "automation-pham" -Location "East US" -ResourceGroupName "UALR-Duc-Pham-rg"
+New-AzureRmAutomationAccount -Name "automation-pham" -Location eastus2 -ResourceGroupName "UALR-Duc-Pham-rg"
